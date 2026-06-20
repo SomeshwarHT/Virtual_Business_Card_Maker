73c7bc5a0ffaf3aade4708212a7426ea21613a66
Jitheshwar H T <jithesh.ht@gmail.com>
2026-06-20 10:04:54 +0530

    Migrated project from SQLite to MySQL

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
